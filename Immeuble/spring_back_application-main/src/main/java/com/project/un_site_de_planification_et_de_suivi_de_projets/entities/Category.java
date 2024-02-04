3071845bcc918dafb518531542b5726e6fb8f01c
unknown <ahmedformed@gmail.com>
2024-02-04 11:55:13 +0100

    back-immeuble

---
+++ Immeuble/spring_back_application-main/src/main/java/com/project/un_site_de_planification_et_de_suivi_de_projets/entities/Category.java
@@ -0,0 +1,64 @@
+package com.project.un_site_de_planification_et_de_suivi_de_projets.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "categorys")
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+
+    @OneToOne
+    Image image;
+
+    public Category(Long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+}
